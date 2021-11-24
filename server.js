f0f62eeb76ac5e398a964143e304a9212a5d80ac
Krushil Naik <krushilnaik96@gmail.com>
2021-11-24 11:52:55 -0500

    Login page (#2)

--- server.js
+++ server.js
@@ -1,32 +1,54 @@
-const path = require("path");
-const express = require("express");
-const app = express();
-const exphbs = require("express-handlebars");
-const session = require("express-session");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
-const sequelize = require("./config/connection");
+const path = require('path');
+const express = require('express');
+const { engine } = require('express-handlebars');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sequelize = require('./config/connection');
+
+/**
+ * @type {session.SessionOptions}
+ */
 const sess = {
-    secret: "I like oreos",
-    cookie: {},
-    resave: false,
-    saveUnitialized: true,
-    store: new SequelizeStore({
-        db: sequelize
-    })
+	secret: 'I like oreos',
+	cookie: {},
+	resave: false,
+	saveUnitialized: true,
+	store: new SequelizeStore({
+		db: sequelize
+	})
 };
-const routes = require("./controllers");
+
 const PORT = process.env.PORT || 3001;
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+const app = express();
+
+// const apiRoutes = require('./controllers/api');
+
+app.engine('handlebars', engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
-app.use(routes);
+app.use(express.static(path.join(__dirname, 'public')));
 
-sequelize.sync({ force: false })
-    .then(() => {
-        app.listen(PORT, () => console.log(`Now listening on ${PORT}!`));
-    });
+// app.use('/api', apiRoutes);
+
+app.get('/login', (req, res) => {
+	res.render('login', {
+		title: 'Budget Better | Login',
+		styles: [{ sheet: 'style' }, { sheet: 'login' }],
+		scripts: [{ script: 'login' }]
+	});
+});
+
+app.get('/', (req, res) => {
+	res.render('index', {
+		title: 'Budget Better',
+		styles: [{ sheet: 'style' }, { sheet: 'index' }]
+	});
+});
+
+sequelize.sync({ force: false }).then(() => {
+	app.listen(PORT, () => console.log(`Now listening on ${PORT}!`));
+});
