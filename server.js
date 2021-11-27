763ab6b68757fbb1cb3dd10449ba8f876aca7e1f
AS <asadrgilany7@gmail.com>
2021-11-27 17:43:02 -0500

    edited server.js and controllers index

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
-const path = require("path");
+const routes = require("./controllers");
 const express = require("express");
-const app = express();
 const exphbs = require("express-handlebars");
+const helpers = require("./utils/helpers");
+const hbs = exphbs.create({ helpers });
+
+const sequelize = require("./config/connection");
+
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
 const session = require("express-session");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
-const sequelize = require("./config/connection");
+
 const sess = {
     secret: "I like oreos",
     cookie: {},
@@ -14,16 +23,17 @@ const sess = {
         db: sequelize
     })
 };
-const routes = require("./controllers");
-const PORT = process.env.PORT || 3001;
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
 
 app.use(session(sess));
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+
 app.use(routes);
 
 sequelize.sync({ force: false })
