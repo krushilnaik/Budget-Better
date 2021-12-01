d81709d6b62d9868390f306d3b3022958e5ca292
Krushil Naik <krushilnaik96@gmail.com>
2021-12-01 18:56:02 -0500

    Frontend (#20)

--- public/assets/js/add-item.js
+++ public/assets/js/add-item.js
@@ -1,29 +1,50 @@
-async function newFormHandler(event) {
-    event.preventDefault();
+async function newFormHandler(event_id, name, price) {
+	const response = await fetch(`/api/items`, {
+		method: 'POST',
+		body: JSON.stringify({
+			name,
+			price,
+			event_id
+		}),
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
 
-    // const itemName = document.querySelector('UPDATE').value;
-    // const itemPrice = document.querySelector('UPDATE').value;
-    // const eventId = window.location.toString().split('/')[
-    //     window.location.toString().split('/').length - 1
-    // ];
-
-    const response = await fetch(`/api/items`, {
-        method: 'POST',
-        body: JSON.stringify({
-            itemName,
-            itemPrice,
-            eventId
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
-
-    if (response.ok) {
-        //document.location.replace('/dashboard');
-    } else {
-        alert(response.statusText);
-    }
+	if (response.ok) {
+		document.location.replace('/dashboard');
+	} else {
+		alert(response.statusText);
+	}
 }
 
-//document.querySelector('UPDATE').addEventListener('submit', newFormHandler);
+// console.log(document.querySelectorAll('.item-wrapper'));
+
+document.querySelectorAll('.item-wrapper').forEach(form => {
+	const button = form.querySelector('button');
+
+	const id = button.id.split('-').splice(-1)[0];
+
+	const name = form.querySelector('input[id^="new-name"]');
+	const price = form.parentElement.querySelector('input[id^="new-price"]');
+
+	name.addEventListener('input', event => {
+		event.preventDefault();
+
+		console.log(name.value);
+	});
+
+	price.addEventListener('input', event => {
+		event.preventDefault();
+
+		console.log(price.value);
+	});
+
+	button.addEventListener('click', event => {
+		event.preventDefault();
+
+		console.log('click registered');
+
+		newFormHandler(Number(id), name.value, Number(price.value));
+	});
+});
