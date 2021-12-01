57e2082da64777c05cf24ad2f41a2d4e3ae6e39c
Krushil Naik <krushilnaik96@gmail.com>
2021-12-01 10:59:23 -0500

    Implement Add Event Form

--- public/assets/js/add-event.js
+++ public/assets/js/add-event.js
@@ -1,28 +1,27 @@
 async function newFormHandler(event) {
-    event.preventDefault();
-  
-    const eventName = document.querySelector('#eventName').value;
-    //const eventDate = document.querySelector('UPDATE').value;
-    //const eventBudget = document.querySelector('UPDATE').value;
-  
-    const response = await fetch(`/api/events`, {
-      method: 'POST',
-      body: JSON.stringify({
-        eventName,
-        eventDate,
-        eventBudget
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-  
-    if (response.ok) {
-      // document.location.replace('/UPDATE');
-    } else {
-      alert(response.statusText);
-    }
-  }
-  
-  // document.querySelector('UPDATE').addEventListener('submit', newFormHandler);
-  
+	event.preventDefault();
+
+	const title = document.querySelector('#eventName').value;
+	const date = document.querySelector('#eventDate').value;
+	const budget = document.querySelector('#eventBudget').value;
+
+	const response = await fetch(`/api/events`, {
+		method: 'POST',
+		body: JSON.stringify({
+			title,
+			date,
+			budget
+		}),
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
+
+	if (response.ok) {
+		document.location.replace('/dashboard');
+	} else {
+		alert(response.statusText);
+	}
+}
+
+document.querySelector('.submit').addEventListener('click', newFormHandler);
