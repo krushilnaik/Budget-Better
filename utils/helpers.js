206c0e545a0fe8e10e7958caf64ad606ce156ad4
AS <asadrgilany7@gmail.com>
2021-12-05 13:54:59 -0500

    not display cents if they're equal to .00

--- utils/helpers.js
+++ utils/helpers.js
@@ -10,7 +10,19 @@ function format_date(date) {
 	return formattedDate;
 };
 
+function format_cents(amount) {
+	let cents = amount.slice(-2);
+	if (cents === "00") {
+		let noCents = amount.split(".")[0];
+		return noCents;
+	}
+	else {
+		return amount;
+	}
+};
+
 module.exports = {
 	format_money,
-	format_date
+	format_date,
+	format_cents
 };
