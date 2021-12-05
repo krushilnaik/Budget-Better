72af2bc808d2f542b29cf4759faef066a72d33b1
Krushil Naik <krushilnaik96@gmail.com>
2021-12-04 19:48:36 -0500

    Style the delete buttons and include toggles

--- public/assets/js/get-subtotal.js
+++ public/assets/js/get-subtotal.js
@@ -1,28 +1,33 @@
 function updateSubtotal() {
-    const allEvents = document.querySelectorAll(".event");
-    allEvents.forEach(event => {
-        let budgetNumber = parseFloat(event.querySelector(".budget").textContent.split("$")[1].trim().replace(",", ""));
-        let budgetEl = event.querySelector(".total");
-        let subtotal = 0;
-        event.querySelectorAll('.item-list .price').forEach(price => {
-            const subprice = Number(price.textContent.split('$')[1].trim());
-            if (price.previousSibling.previousSibling.checked){
-                subtotal+=subprice;
-            }
-        });
-        if (subtotal > budgetNumber) {
-            budgetEl.classList.add("over-budget");
-        }
-        else {
-            budgetEl.classList.remove("over-budget");
-        }
+	const allEvents = document.querySelectorAll('.event');
+	allEvents.forEach(event => {
+		let budgetNumber = parseFloat(
+			event
+				.querySelector('.budget')
+				.textContent.split('$')[1]
+				.trim()
+				.replace(/,/g, '')
+		);
+		let budgetEl = event.querySelector('.total');
+		let subtotal = 0;
+		event.querySelectorAll('.item-list .price').forEach(price => {
+			const subprice = Number(price.textContent.split('$')[1].trim());
+			if (price.previousElementSibling.previousElementSibling.checked) {
+				subtotal += subprice;
+			}
+		});
+		if (subtotal > budgetNumber) {
+			budgetEl.classList.add('over-budget');
+		} else {
+			budgetEl.classList.remove('over-budget');
+		}
 
-        budgetEl.textContent = 'Current subtotal: $ ' + subtotal;
-    })
+		budgetEl.textContent = 'Current subtotal: $ ' + subtotal;
+	});
 }
 
-const checkboxEl = document.querySelectorAll(".include");
+const checkboxEl = document.querySelectorAll('.include');
 checkboxEl.forEach(element => {
-    element.addEventListener("change", updateSubtotal);
-})
+	element.addEventListener('change', updateSubtotal);
+});
 updateSubtotal();
